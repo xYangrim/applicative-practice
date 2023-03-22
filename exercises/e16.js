9b46a567e2d7c96cec660615371da00ac59b4796
Andrey Codez <andreycodez@bixtrix.com>
2023-03-22 17:02:53 +0500

    Update e16 exercise
    Add task description and requirements. add the Test yourself part.

--- exercises/e16.js
+++ exercises/e16.js
@@ -1,3 +1,23 @@
-// see e16.md
+/**
+ * Make sure to read the e16.md file!
+ * For this exercise we want you to make a custom `find` method. 
+ * In order to do this it is ok to use any type of FOR loops.
+ * REQUIREMENT: A find function should
+ * * Take an array of things
+ * * Return a the first thing where the applied callback is truthey
+ * The function MUST NOT use the find() method.
+ */
 
-export function find(array, callback) {}
+
+export function find(array, callback) {
+  // Your code goes here...
+  for (let item of array) {
+    if (callback(item)) return item;
+  }
+}
+
+
+// === TEST YOURSELF ===
+// Once you're finished run the test with "npm run test-16"
+// If the test has all tests passed, switch to the next exercise file
+// If any of the tests fails, refactor the code and run the test command after you've fixed the function
