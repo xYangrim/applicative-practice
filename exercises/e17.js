0c3fc98298cc54e726822c00c2103519e0d730bd
Andrey Codez <andreycodez@bixtrix.com>
2023-03-22 17:11:00 +0500

    Update e17 exercise
    Adde Exercise description and the Test yoursel part

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,3 +1,25 @@
-export function minBy(array, cb) {}
+/**
+ * Make sure to read the e17.md file!
+ * For this exercise we want you to make custom `maxBy` and `minBy` methods. 
+ * In order to do this it is ok to use any type of FOR loops. 
+ * REQUIREMENTS: functions should:
+ * * Take an array of things
+ * * minBy(): Return the element where the callback function on that element yields the lowest value
+ * * maxBy(): Return the element where the callback function on that element yields the highest value
+ */
 
-export function maxBy(array, cb) {}
+export function minBy(array, cb) {
+  // Your code goes here...
+
+}
+
+export function maxBy(array, cb) {
+  // Your code goes here...
+
+}
+
+
+// === TEST YOURSELF ===
+// Once you're finished run the test with "npm run test-17"
+// If the test has all tests passed, switch to the next exercise file
+// If any of the tests fails, refactor the code and run the test command after you've fixed the function
